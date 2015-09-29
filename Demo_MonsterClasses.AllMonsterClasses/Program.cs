e34b7a64bfb682a25c4fb359bd0a79fcfff3488e
John Velis <john@johnvelis.com>
2015-09-29 14:58:16 -0400

    initial commit

---
+++ Demo_MonsterClasses.AllMonsterClasses/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MonsterClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            // instantiate (create) a sea monster and call a method 
+            // to set the attributes
+            //
+            SeaMonster seymore = new SeaMonster();
+            SetSeaMonsterAttributes(seymore);
+
+            //
+            // instantiate (create) a sea monster and use an
+            // object intializer to set the attributes
+            //
+            SeaMonster suzy = new SeaMonster
+            {
+                Name = "Suzy the Sea Monster",
+                Type = "Big, Slimy Fish",
+                Sea = "The Baltic Sea",
+                HasGills = true,
+                NumbeOfLegs = 8
+            };
+
+            //
+            // instantiate (create) a space monster and use an
+            // object intializer to set the attributes
+            //
+            SpaceMonster sid = new SpaceMonster
+            {
+                Name = "Sid the Space Monster",
+                Type = "Billowng Floater",
+                Planet = "The Gasous Giant Emcore",
+                HasDeathRay = false,
+                NumbeOfWings = 2
+            };
+
+            //
+            // display the monsters' attributes
+            //
+            seymore.DisplaySeaMonsterInfo();
+            Console.WriteLine();
+            suzy.DisplaySeaMonsterInfo();
+            Console.WriteLine();
+            sid.DisplaySpaceMonsterInfo();
+
+            //
+            // pause the console window and wait for a keystroke
+            //
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// set the monster attributes using a method
+        /// </summary>
+        /// <param name="monster"></param>
+        private static void SetSeaMonsterAttributes(SeaMonster monster)
+        {
+            //
+            // set the sea monsters atttibutes
+            //
+            monster.Name = "Seymore the Sea Monster";
+            monster.Type = "Big, Slimy Fish";
+            monster.Sea = "The Baltic Sea";
+            monster.HasGills = true;
+            monster.NumbeOfLegs = 7;
+            monster.Friends[0] = "Suzy";
+        }
+    }
+}
---
+++ Demo_MonsterClasses.AllMonsterClasses/SpaceMonster.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MonsterClasses
+{
+    class SpaceMonster
+    {
+
+        #region ENUMERABLES
+
+        public enum DispositionType
+        {
+            Happy,
+            Sad,
+            Nice,
+            Mean,
+            Crazy
+        }
+
+        #endregion
+
+        #region FIELDS
+
+        private string _name;
+        private string _type;
+        private string _planet;
+
+        private bool _hasDeathRay;
+        private int _numberOfWings;
+
+        private DispositionType _disposition;
+
+        private string[] _friends = new string[5];
+
+
+        #endregion
+
+        #region PROPERTIES
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public string Planet
+        {
+            get { return _planet; }
+            set { _planet = value; }
+        }
+
+        public bool HasDeathRay
+        {
+            get { return _hasDeathRay; }
+            set { _hasDeathRay = value; }
+        }
+
+        public int NumbeOfWings
+        {
+            get { return _numberOfWings; }
+            set { _numberOfWings = value; }
+        }
+
+        public DispositionType Disposition
+        {
+            get { return _disposition; }
+            set { _disposition = value; }
+        }
+
+        public string[] Friends
+        {
+            get { return _friends; }
+            set { _friends = value; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public SpaceMonster()
+        {
+
+        }
+
+        public SpaceMonster(string name)
+        {
+            _name = name;
+        }
+
+        public SpaceMonster(string name, DispositionType disposition)
+        {
+            _name = name;
+            _disposition = disposition;
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// display all of the monster's attributes
+        /// </summary>
+        public void DisplaySpaceMonsterInfo()
+        {
+            Console.WriteLine("Greetings - My Monster's Attributes");
+            Console.WriteLine();
+
+            Console.WriteLine("Name: {0}", _name);
+            Console.WriteLine("Type: {0}", _type);
+            Console.WriteLine("Planet: {0}", _planet);
+            Console.WriteLine("Has Death Ray: {0}", _hasDeathRay);
+            Console.WriteLine("Number or Wings: {0}", _numberOfWings);
+            Console.WriteLine("Disposition: {0}", Disposition);
+
+            Console.WriteLine();
+            Console.WriteLine("Friends");
+            foreach (var friend in _friends)
+            {
+                if (friend != null)
+                {
+                Console.WriteLine("Friend: {0}", friend);                    
+                }
+            }
+        }
+
+        #endregion
+    }
+}
